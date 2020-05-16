8c5235fa70299c6777ab5f2d09c74c40db5f7850
Andrea Leofreddi <aleofreddi@google.com>
2020-05-16 22:32:46 +0200

    Make mock using mockgen

---
+++ common.mk
@@ -0,0 +1,13 @@
+VERSION?=$(shell git describe --tags 2>/dev/null || (printf commit-; git rev-parse --short HEAD))
+
+%.pb.go: %.proto
+	protoc --go_out=plugins=grpc:. $<
+
+mock/%.int.go: %.go
+	mockgen -package mock -destination $@ -source $<
+
+mock/%.ext.go: %.mock
+	mockgen -package mock -destination $@ `cat $<`
+
+%.pb.go: %.proto
+	protoc --go_out=plugins=grpc:. $<
