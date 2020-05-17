ed47c2b5ae1a8b1513945792f61aacbc9b3ca6f6
Andrea Leofreddi <aleofreddi@google.com>
2020-05-17 17:43:32 +0200

    Move go dependency download to a separate target

--- common.mk
+++ common.mk
@@ -1,11 +1,15 @@
+.PHONY: godep
+
 VERSION?=$(shell git describe --tags 2>/dev/null || (printf commit-; git rev-parse --short HEAD))
 
 proto/%.pb.go: %.proto
 	protoc --go_out=plugins=grpc:. --go_opt=paths=source_relative $<
-#	protoc --go_out=plugins=grpc:. --go_out=proto --go_opt=paths=source_relative $<
 
 mock/%.mi.go: %.go
 	mockgen -package mock -destination $@ -source $<
 
 mock/%.me.go: %.mock
 	mockgen -package mock -destination $@ `cat $<`
+
+godep:
+	go get ./...
